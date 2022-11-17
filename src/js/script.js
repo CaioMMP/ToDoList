0f6ca83772526260b41014975b71bc10088e81d3
Kuuhakuanimes <mamedemacielpassos@gmail.com>
2022-11-17 00:00:30 -0300

    Versão3.1 - Validação de Cadastro por JS funcionando/Nova página para mostrar dados(Inacabada)

--- src/js/script.js
+++ src/js/script.js
@@ -1,19 +1,20 @@
+var nome;
+var login;
+var email;
+var senha;
+var confirmSenha;
 function cadastra() {
-    var nome = document.getElementById("nome").value;
-    var login = document.getElementById("login").value;
-    var email = document.getElementById("email").value;
-    var senha = document.getElementById("senha").value;
-    var confirmSenha = document.getElementById("confirmSenha").value;
-    if ((nome.search(" ") == -1) &&
-                (login.search(" ") == -1) &&
-                (email.search(" ") == -1) &&
-                (senha.search(" ") == -1) &&
-                (confirmSenha.search(" ") == -1) &&
-                (email.search("@") == -1) &&
-                (email.search(".") != -1)) {
-        alert("!!!Campos Vazios!!!");
+    nome = document.getElementById("nome").value;
+    login = document.getElementById("login").value;
+    email = document.getElementById("email").value;
+    senha = document.getElementById("senha").value;
+    confirmSenha = document.getElementById("confirmSenha").value;
+    if (email.search("@") == -1) {
+        alert("ERROR! - E-mail inválido_Verifique se há '@'");
+    } else if ((senha == "") || (nome == "") || (login == "") || (email == "") || (confirmSenha == "")) {
+        alert("ERROR! - Campos Vazios");
     } else if (senha != confirmSenha) {
-        alert("!!!Senha e Confirmação da Senha devem ser iguais!!!");
+        alert("ERROR! - Senha e Confirmação da Senha devem ser iguais");
     } else {
         window.location.href = "toDoList.html";
     }
